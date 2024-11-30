9c1cab2beed916d7031239d22f48d7fa15dbc514
TimbakTwo <hilencesilence@gmail.com>
2024-11-30 21:41:38 +0800

    Delete personal portfolio page directory

--- personal portfolio page/js/script.js
+++
@@ -1,9 +0,0 @@
- // JavaScript to detect scroll position and adjust navbar style
-  window.addEventListener("scroll", () => {
-    const navmenu = document.querySelector(".nav-menu");
-    if (window.scrollY > 0) {
-      navmenu.classList.add("scrolled");
-    } else {
-      navmenu.classList.remove("scrolled");
-    }
-  });
